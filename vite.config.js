557c109592387b345a90df257dacbb8fb81c8ec8
Olakunle Samuel <olakunleolawale21@gmail.com>
2026-01-02 22:21:56 +0100

    change made

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/Olawale-Portfolio',
   css: {
     modules : {
       localsConvention: 'camelCase'
